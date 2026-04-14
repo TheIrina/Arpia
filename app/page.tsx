1873e06a799444f86af284561345a1beeb8cfbfd
rubiuscoffee <bsquintero_usistemas@intep.edu.co>
2026-04-14 13:46:09 -0500

    Add WhoWeAre section and GSAP dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "./(landing)/components/hero";
 import { Navbar } from "./(landing)/components/navbar";
+import { WhoWeAre } from "./(landing)/whoweare/who";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#fefefe]">
       <Navbar />
       <Hero />
+      <WhoWeAre />
     </main>
   );
 }
